c7019e04323561d55e3b70d95922e078ff8a5d91
vasily <vasily@0c492187-f2f6-b544-b8a9-ed9c5deff83c>
2011-03-15 21:45:33 +0000

    FlowRestoratorTest changed. at the moment the problem is explicitly shown (negative values in result)
    FlowRestorator.java - need to debug
    
    git-svn-id: file:///media/5E5CA8755CA8499B/svn_repository/SolverHelper/trunk@13 0c492187-f2f6-b544-b8a9-ed9c5deff83c

--- ru/vasily/test/FlowRestoratorTest.java
+++ ru/vasily/test/FlowRestoratorTest.java
@@ -2,27 +2,52 @@ package ru.vasily.test;
 
 import java.util.Arrays;
 
+import com.google.common.base.Objects;
+
 import ru.vasily.solver.FlowRestorator;
 
 public class FlowRestoratorTest {
 
 	public static void main(String[] args) {
+		double[] input = new double[] { 1.0, 2.0, 3.0, 5.0, 100, 0.5, 3, 5, 78 };
+
+		FlowRestorator.Fetcher fetcher = getFetcher(input);
+		FlowRestorator restorator = new FlowRestorator(fetcher, 1, 1, 1);
+		double[] resultL = new double[input.length];
+		double[] resultR = new double[input.length];
+		setResult(restorator, resultL, resultR);
+		String out = Objects.toStringHelper("result")
+				.add("resultL", Arrays.toString(resultL))
+				.add("resultR", Arrays.toString(resultR)).toString();
+		System.out.println(out);
+	}
+
+	private static void setResult(FlowRestorator restorator, double[] resultL, double[] resultR) {
+		double[] ul = new double[1];
+		double[] ur = new double[1];
+		for (int i = 0; i < resultL.length; i++) {
+			try {
+				restorator.setRestoredURandUL(ur, ul, i);
+				resultL[i] = ul[0];
+				resultR[i] = ur[0];
+			} catch (ArrayIndexOutOfBoundsException e) {
+			}
+		}
+	}
+
+	private static FlowRestorator.Fetcher getFetcher(final double[] array) {
 		FlowRestorator.Fetcher fetcher = new FlowRestorator.Fetcher() {
 
 			@Override
 			public void setU(double[] arr, int i) {
-				arr[0] = 1;
+				arr[0] = array[i];
 			}
 
 			@Override
 			public void setDelta(double[] arr, int i) {
-				arr[0] = 0;
+				arr[0] = array[i + 1] - array[i];
 			}
 		};
-		FlowRestorator restorator = new FlowRestorator(fetcher, 1, 1, 1);
-		double[] ul = new double[1];
-		double[] ur = new double[1];
-		restorator.setRestoredURandUL(ur, ul, 0);
-		System.out.println(Arrays.toString(ul) + " " + Arrays.toString(ur));
+		return fetcher;
 	}
 }
