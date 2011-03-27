f53f7ec512c3b625f1e4d6aec03b084157ef4f66
vasily <vasily@0c492187-f2f6-b544-b8a9-ed9c5deff83c>
2011-03-27 16:07:10 +0000

    delete trash
    git-svn-id: file:///media/5E5CA8755CA8499B/svn_repository/SolverHelper/trunk@30 0c492187-f2f6-b544-b8a9-ed9c5deff83c

--- input_files/start.bat
+++
@@ -1,1 +0,0 @@
-java -cp "..\lib\guava-r07.jar";"..\lib\jackson-core-lgpl-1.7.3.jar";"..\lib\jackson-mapper-lgpl-1.7.3.jar";..\bin ru.vasily.solverhelper.ApplicationMain ../input ../output ../template
