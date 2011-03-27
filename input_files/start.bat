3fd702230b58b1dec1f1441e02b71ddecad145f9
vasily <vasily@0c492187-f2f6-b544-b8a9-ed9c5deff83c>
2011-03-27 16:13:57 +0000

    input folder template
    git-svn-id: file:///media/5E5CA8755CA8499B/svn_repository/SolverHelper/trunk@31 0c492187-f2f6-b544-b8a9-ed9c5deff83c

---
+++ input_files/start.bat
@@ -0,0 +1,1 @@
+java -cp "..\lib\guava-r07.jar";"..\lib\jackson-core-lgpl-1.7.3.jar";"..\lib\jackson-mapper-lgpl-1.7.3.jar";..\bin ru.vasily.solverhelper.ApplicationMain ../input ../output ../template
