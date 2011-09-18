89d1e5808b6fbdf6c5a801a2061a9c942e60ff21
vasily <vasily@0c492187-f2f6-b544-b8a9-ed9c5deff83c>
2011-09-18 14:26:36 +0000

    input files updated
    git-svn-id: file:///media/5E5CA8755CA8499B/svn_repository/SolverHelper/trunk@96 0c492187-f2f6-b544-b8a9-ed9c5deff83c

--- input_files/run_macro_and_shutdown.bat
+++ input_files/run_macro_and_shutdown.bat
@@ -1,1 +1,1 @@
-java -cp "..\lib\guava-r07.jar";"..\lib\jackson-core-lgpl-1.7.3.jar";"..\lib\jackson-mapper-lgpl-1.7.3.jar";..\bin ru.vasily.solverhelper.ApplicationMain ../input ../output ../template s m
+java -cp "..\lib\guava-r09.jar";"..\lib\jackson-core-lgpl-1.7.3.jar";"..\lib\jackson-mapper-lgpl-1.7.3.jar";..\bin ru.vasily.solverhelper.ApplicationMain ../input ../output ../template s m
