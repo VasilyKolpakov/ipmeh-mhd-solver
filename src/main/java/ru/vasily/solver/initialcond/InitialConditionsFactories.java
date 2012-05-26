909d352afd14f06c3cc3313b801c19c305a2e1e0
Vasily <vasilykolpakov@gmail.com>
2012-05-27 02:09:38 +0400

    alternative steady shock initial conditions added

--- src/main/java/ru/vasily/solver/initialcond/InitialConditionsFactories.java
+++ src/main/java/ru/vasily/solver/initialcond/InitialConditionsFactories.java
@@ -3,7 +3,9 @@ package ru.vasily.solver.initialcond;
 import com.google.common.collect.ImmutableMap;
 import ru.vasily.core.dataobjs.DataObject;
 import ru.vasily.solver.MHDValues;
+import ru.vasily.solver.ShockJump;
 import ru.vasily.solver.Utils;
+import ru.vasily.solver.utils.SteadyShockByKryukov;
 
 import java.util.Map;
 
@@ -18,6 +20,7 @@ public class InitialConditionsFactories
                     put("orsag_tang_vortex", new OrsagTangVortex()).
                     put("kelvin_helmholtz", new KelvinHelmholtz()).
                     put("steady_shock", new SteadyShock()).
+                    put("steady_shock_alt", new SteadyShockAlternative()).
                     build();
 
     public static class FillRect implements Function2DFactory
@@ -28,8 +31,8 @@ public class InitialConditionsFactories
         {
             double[] val = parseConservativeVals(data, physicalConstants);
             return new FillSquareFunction(val, data.getDouble("x1"),
-                    data.getDouble("y1"), data.getDouble("x2"),
-                    data.getDouble("y2"));
+                                          data.getDouble("y1"), data.getDouble("x2"),
+                                          data.getDouble("y2"));
         }
     }
 
@@ -104,11 +107,38 @@ public class InitialConditionsFactories
         }
     }
 
+    public static class SteadyShockAlternative implements Function2DFactory
+    {
+
+        @Override
+        public Init2dFunction createFunction(DataObject conditionsData, DataObject physicalConstants)
+        {
+            double gamma = physicalConstants.getDouble("gamma");
+            double absVelocityL = conditionsData.getDouble("abs_v");
+            double velocityAngleL = conditionsData.getDouble("v_angle");
+            double absBL = conditionsData.getDouble("abs_b");
+            double BAngleL = conditionsData.getDouble("b_angle");
+            double pressureRatio = conditionsData.getDouble("p_ratio");
+            double machNumber = conditionsData.getDouble("mach");
+
+
+            ShockJump jump = SteadyShockByKryukov.steadyShockByKryukov(absVelocityL,
+                                                                       velocityAngleL,
+                                                                       absBL,
+                                                                       BAngleL,
+                                                                       gamma,
+                                                                       pressureRatio,
+                                                                       machNumber);
+            return new HorizontalShockFunction(jump.left, jump.right, conditionsData.getDouble("x_s"), gamma);
+        }
+    }
+
+
     private static double[] parseConservativeVals(DataObject data, DataObject physicalConstants)
     {
         double[] val = new double[8];
         Utils.setConservativeValues(data.getObj("value"), val,
-                physicalConstants.getDouble("gamma"));
+                                    physicalConstants.getDouble("gamma"));
         return val;
     }
 
